141089a4c640e1821297236d47be95f0a9b67a67
alok <alok@alok-PC>
2018-08-07 22:12:01 +0530

    Third commite

---
+++ JavaProgranning/src/com/java/dataStructure/MyTest3.java
@@ -0,0 +1,12 @@
+package com.java.dataStructure;
+
+public class MyTest3 {
+
+	public static void main(String[] args) {
+		
+		String s=new String("Alok Ranjan Welcome to Bihar");
+		
+		System.out.println(s);
+	}
+
+}
