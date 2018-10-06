d9d06a886708cc8aed2a7148496cf5263adf7cf1
alok <alok@alok-PC>
2018-10-06 13:15:29 +0530

    hello

--- JavaProgranning/src/com/java/dataStructure/Alol.java
+++ JavaProgranning/src/com/java/dataStructure/Alol.java
@@ -3,5 +3,6 @@ package com.java.dataStructure;
 public class Alol {
 public static void main(String[] args) {
 	System.out.println("Hello Alok Welcome to java programming");
+	System.out.println("Hello Alok Welcome ");
 }
 }
